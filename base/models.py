c21458f9f4ae9e0e46000b43230745b7107b8136
Nninex <kakhniashvinini@gmail.com>
2024-04-13 18:32:00 +0400

    little changes

--- base/models.py
+++ base/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Category(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default = 1)
     name = models.CharField(max_length=50)
 
     def __str__(self):
